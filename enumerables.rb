3d08722a0894cee95d1ab66dee92f74644ff17cc
evansinho <igiri.evanson@gmail.com>
2020-04-30 14:03:48 +0100

    comment out the testings

--- enumerables.rb
+++ enumerables.rb
@@ -123,54 +123,54 @@ module Enumerable
 end
 # rubocop:enable Metrics/PerceivedComplexity,Metrics/CyclomaticComplexity
 
-puts 'my_each vs. each'
-[1, 2, 3, 4, 5].my_each { |item| print item * 2 }
-puts ''
-[1, 2, 3, 4, 5].each { |item| print item * 2 }
-puts ''
+# puts 'my_each vs. each'
+# [1, 2, 3, 4, 5].my_each { |item| print item * 2 }
+# puts ''
+# [1, 2, 3, 4, 5].each { |item| print item * 2 }
+# puts ''
 
-puts 'my_each_with_index vs. each_with_index'
-[1, 2, 3, 4, 5].my_each_with_index { |item, index| print [item, index] }
-puts ''
-[1, 2, 3, 4, 5].each_with_index { |item, index| print [item, index] }
-puts ''
+# puts 'my_each_with_index vs. each_with_index'
+# [1, 2, 3, 4, 5].my_each_with_index { |item, index| print [item, index] }
+# puts ''
+# [1, 2, 3, 4, 5].each_with_index { |item, index| print [item, index] }
+# puts ''
 
-puts 'my_select vs. select'
-print [1, 2, 3, 4, 5].my_select(&:even?)
-puts ''
-print [1, 2, 3, 4, 5].select(&:even?)
-puts ''
+# puts 'my_select vs. select'
+# print [1, 2, 3, 4, 5].my_select(&:even?)
+# puts ''
+# print [1, 2, 3, 4, 5].select(&:even?)
+# puts ''
 
-puts 'my_all? vs. all?'
-puts [1, 2, 3, 4, 5].my_all? { |num| num < 10 }
-puts [1, 2, 3, 4, 5].all? { |num| num < 10 }
+# puts 'my_all? vs. all?'
+# puts [1, 2, 3, 4, 5].my_all? { |num| num < 10 }
+# puts [1, 2, 3, 4, 5].all? { |num| num < 10 }
 
-puts 'my_any? vs. any?'
-puts [1, 2, 3, 4, 5].my_any? { |num| num > 3 }
-puts [1, 2, 3, 4, 5].any? { |num| num > 3 }
+# puts 'my_any? vs. any?'
+# puts [1, 2, 3, 4, 5].my_any? { |num| num > 3 }
+# puts [1, 2, 3, 4, 5].any? { |num| num > 3 }
 
-puts 'my_none? vs. none?'
-puts [1, 2, 3, 4, 5].my_none? { |num| num > 3 }
-puts [1, 2, 3, 4, 5].none? { |num| num > 3 }
+# puts 'my_none? vs. none?'
+# puts [1, 2, 3, 4, 5].my_none? { |num| num > 3 }
+# puts [1, 2, 3, 4, 5].none? { |num| num > 3 }
 
-puts 'my_count vs. count'
-puts [1, 2, 3, 4, 5].my_count(&:even?)
-puts [1, 2, 3, 4, 5].my_count
-puts [1, 2, 3, 4, 5].count(&:even?)
-puts [1, 2, 3, 4, 5].count
+# puts 'my_count vs. count'
+# puts [1, 2, 3, 4, 5].my_count(&:even?)
+# puts [1, 2, 3, 4, 5].my_count
+# puts [1, 2, 3, 4, 5].count(&:even?)
+# puts [1, 2, 3, 4, 5].count
 
-puts 'my_map vs. map'
-print [1, 2, 3, 4, 5].my_map { |num| num * 2 }
-puts ''
-print [1, 2, 3, 4, 5].map { |num| num * 2 }
-puts ''
+# puts 'my_map vs. map'
+# print [1, 2, 3, 4, 5].my_map { |num| num * 2 }
+# puts ''
+# print [1, 2, 3, 4, 5].map { |num| num * 2 }
+# puts ''
 
-puts 'my_inject vs. inject'
-puts [1, 2, 3, 4, 5].my_inject { |sum, num| sum * num }
-puts [1, 2, 3, 4, 5].inject { |sum, num| sum * num }
-puts 'initial = 2'
-puts [1, 2, 3, 4, 5].my_inject(2) { |sum, num| sum * num }
-puts [1, 2, 3, 4, 5].inject(2) { |sum, num| sum * num }
+# puts 'my_inject vs. inject'
+# puts [1, 2, 3, 4, 5].my_inject { |sum, num| sum * num }
+# puts [1, 2, 3, 4, 5].inject { |sum, num| sum * num }
+# puts 'initial = 2'
+# puts [1, 2, 3, 4, 5].my_inject(2) { |sum, num| sum * num }
+# puts [1, 2, 3, 4, 5].inject(2) { |sum, num| sum * num }
 
-puts 'multiply_els'
-puts multiply_els([2,4,5])
+# puts 'multiply_els'
+# puts multiply_els([2,4,5])
